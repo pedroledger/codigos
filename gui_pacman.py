999585702e34ced6e7f0d36c30143372f8f2a1f4
pedroledger <62995752+pedroledger@users.noreply.github.com>
2020-08-17 18:50:26 -0300

    Update gui_pacman.py

--- gui_pacman.py
+++ gui_pacman.py
@@ -1,53 +1,57 @@
-from turtle import *
+from turtle import * # IMPORTA A BIBLIOTECA TURTLE
 
 
-pedro = Turtle(visible=False)
+pedro = Turtle(visible=False) # TARTARUGA QUE VAI DESENHAR AS CÉLULAS
+
 matriz = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
-    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
-    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,
-    0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
 ]
 def ler_matriz(matriz):
-    # IMPLEMENTE A LÓGICA A SEGUIR
-    return matriz # uma matriz fixa
+    # A PARTIR DE UMA MATRIZ BIDIMENSIONAL (LISTA DE LISTAS), DE TAMANHO 20x20, RETORNA UMA LISTA SÓ
+    matriz_lida = []
+    for linha in matriz:
+        for coluna in linha:
+            j = linha[coluna]
+            matriz_lida.append(j)
+    return matriz_lida 
 
 def desenhar_celula(coordenada, cor='grey'):
     """ Dada uma coordenada (xt, yt) do Turtle, desenha um quadrado (célula) na posição """
-    # IMPLEMENTE A LÓGICA ESPECIFICADA AQUI
-    pedro.color(cor)
+    pedro.color(cor) # A TARTARUGA RECEBE POR PADRÃO A COR CINZA, MAS PODE SER ALTERADA
     pedro.speed(0)
     pedro.up()
-    pedro.goto(coordenada)
+    pedro.goto(coordenada) # VAI ATÉ A COORDENADA, QUE É PASSADA EM FORMATO DE TUPLA
     pedro.down()
     pedro.begin_fill()
 
     for i in range(4):
-        pedro.forward(20)
+        pedro.forward(20) # DESENHA QUADRADOS DE TAMANHO 20x20
         pedro.left(90)
 
     pedro.end_fill()
 
 def em_coord_turtle(matriz):
     """ Dada uma coordenada da matriz (i,j) transforma em coordenada Turtle """
-    # IMPLEMENTE A LÓGICA ESPECIFICADA AQUI
-    # 'i' representa a linha e 'j' a coluna
+    # RECEBE UMA LISTA COM OS VALORES DA MATRIZ E TRANSFORMA EM UMA LISTA COM COORDENADAS DE SEUS RESPECTIVOS VALORES
     celula = []
     coords = []
     for index in range(len(matriz)):
@@ -62,17 +66,19 @@ def em_coord_turtle(matriz):
 
 def criar_labirinto(p1=420, p2=420, p3=370, p4=0):
     """ Cria uma tela do Turtle """
-    bgcolor('black')
+    bgcolor('black') # COR DE FUNDO PRETA
     setup(p1, p2, p3, p4)
     # IMPLEMENTE A LÓGICA A SEGUIR
     # Para cada celula (i,j) da matriz que for caminho desenhe uma celula
-    ler_matriz(matriz)
-    coords = em_coord_turtle(matriz)
+    matriz_lida = ler_matriz(matriz)
+    coords = em_coord_turtle(matriz_lida)
     for cod in coords:
         desenhar_celula(cod)
       
-def main():
+
+
+def main():    
     criar_labirinto()
     done()
-
+    
 main()
